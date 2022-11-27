b1dbbac093e071b3a467dec10a80b8df7c0fb084
Ventura <marlenp363@gmail.com>
2022-11-27 13:39:58 -0500

    primer commit

---
+++ APITecsup/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APITecsup.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET: api/Product
+        public IEnumerable<Producto> Get()
+        {
+            List<Producto> products = new List<Producto>();
+            products.Add(new Producto {
+                Nombre= "Laptop",
+                Descripcion= "Laptop 2",
+                PrecioVenta= 1000.0,
+                EstaActivo= true
+            });
+            return products;
+        }
+
+        // GET: api/Product/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Product/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ APITecsup/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APITecsup.Models
+{
+    public class Product
+    {
+        public int ProductoID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double PrecioVenta { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool EstaActivo { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public double IGV { get; set; }
+    }
+}
